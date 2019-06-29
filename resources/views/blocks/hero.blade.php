a0ad6be43dc8b3dbdf89a619a6ce0c0badf1a40b
Erich Garcia Cruz <erichjosegarcia@gmail.com>
2019-06-28 23:00:18 -0400

    Global View::share in AppServiceProvider

--- resources/views/blocks/hero.blade.php
+++ resources/views/blocks/hero.blade.php
@@ -5,48 +5,42 @@
         <div class="row d-flex justify-content-center">
             <div class="col-md-9 col-lg-9 col-xs-12 text-center">
                 <div class="contents">
-                    <h1 class="head-title">Busca entre +{{ $total_ads }} anuncios o <a href="#">Publica GRATIS</a></h1>
-                    <p>Buy And Sell Everything From Used Cars To Mobile Phones And Computers</p>
+                    <h1 class="head-title">Busca anuncios entre +{{ $total_ads }} en Cuba</h1>
+                    <p>Compra y Vende Computadoras, Celulares, Casas, Carros, ofertas de empleo y todo lo que quieras en Cuba</p>
                     <div class="search-two-form">
                         <form class="search-two" action="">
                             <div class="search-inner">
                                 <div class="row">
-                                    <div class="col-lg-5 col-md-5">
+                                    <div class="col-lg-9 col-md-9">
                                         <div class="form-group search-query">
-                                            <input type="text" name="query" class="form-control" placeholder="What are you looking for?" autocomplete="off">
+                                            <input type="text" name="query" class="form-control" placeholder="Qué estas buscando hoy" autocomplete="off">
                                             <div class="search-suggestion">
                                                 <div class="search-suggestion-items">
                                                     <ul>
-                                                        <li><a href="#"><i class="lni-display"></i> Electronics</a></li>
-                                                        <li><a href="#"><i class="lni-tshirt"></i> Fashion</a></li>
-                                                        <li><a href="#"><i class="lni-mobile"></i> Mobiles</a></li>
-                                                        <li><a href="#"><i class="lni-paint-roller"></i> Services</a></li>
+                                                        <li><a href="#"><i class="lni-display"></i> Computadoras</a></li>
+                                                        <li><a href="#"><i class="lni-tshirt"></i> Ropa</a></li>
+                                                        <li><a href="#"><i class="lni-mobile"></i> Celulares</a></li>
+                                                        <li><a href="#"><i class="lni-paint-roller"></i> Servicios</a></li>
                                                     </ul>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="col-lg-4 col-md-4">
-                                        <div class="form-group search-location">
-                                            <input type="text" name="location" class="form-control" placeholder="Location" autocomplete="off">
-                                            <i class="lni-target"></i>
-                                        </div>
-                                    </div>
                                     <div class="col-lg-3 col-md-3">
-                                        <button class="btn btn-common search-two-submit" type="submit" form="search-two"><i class="lni-search"></i> Search</button>
+                                        <button class="btn btn-common search-two-submit" type="submit" form="search-two"><i class="lni-search"></i> Buscar</button>
                                     </div>
                                 </div>
                             </div>
                         </form>
 
                         <div class="adverts-search-suggestion">
-                            <span class="search-suggestion-title">Popular Searchs</span>
+                            <span class="search-suggestion-title">Búsquedas populares</span>
                             <div class="adverts-search-suggestion-items">
                                 <ul>
-                                    <li><a href="#">Electronics</a></li>
-                                    <li><a href="#">Fashion</a></li>
-                                    <li><a href="#">Mobiles</a></li>
-                                    <li><a href="#">Services</a></li>
+                                    <li><a href="#">dynamic</a></li>
+                                    <li><a href="#">bachecubano motos</a></li>
+                                    <li><a href="#">revolico celulares</a></li>
+                                    <li><a href="#">base de datos ETECSA</a></li>
                                 </ul>
                             </div>
                         </div>
