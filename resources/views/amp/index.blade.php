91eee95a2bfe68a1e65ba0a7b9cbfc3e0cf47d69
Erich Garcia Cruz <erichjosegarcia@gmail.com>
2019-07-02 22:13:17 -0400

    begon AMP support

---
+++ resources/views/amp/index.blade.php
@@ -0,0 +1,140 @@
+<!doctype html>
+<html ⚡ lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,minimum-scale=1">
+    <meta name="description" content="This is the AMP Boilerplate.">
+    <link rel="preload" as="script" href="https://cdn.ampproject.org/v0.js">
+    <link rel="preload" href="hero-img.jpg" as="image">
+    <link rel="preconnect dns-prefetch" href="https://fonts.gstatic.com/" crossorigin>
+    <script async src="https://cdn.ampproject.org/v0.js"></script>
+    <!-- Import other AMP Extensions here -->
+    <script async custom-element="amp-analytics" src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <style amp-custom>
+        /* Add your styles here */
+    </style>
+    <style amp-boilerplate>
+        body {
+            -webkit-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+            -moz-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+            -ms-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+            animation: -amp-start 8s steps(1, end) 0s 1 normal both
+        }
+
+        @-webkit-keyframes -amp-start {
+            from {
+                visibility: hidden
+            }
+
+            to {
+                visibility: visible
+            }
+        }
+
+        @-moz-keyframes -amp-start {
+            from {
+                visibility: hidden
+            }
+
+            to {
+                visibility: visible
+            }
+        }
+
+        @-ms-keyframes -amp-start {
+            from {
+                visibility: hidden
+            }
+
+            to {
+                visibility: visible
+            }
+        }
+
+        @-o-keyframes -amp-start {
+            from {
+                visibility: hidden
+            }
+
+            to {
+                visibility: visible
+            }
+        }
+
+        @keyframes -amp-start {
+            from {
+                visibility: hidden
+            }
+
+            to {
+                visibility: visible
+            }
+        }
+    </style><noscript>
+        <style amp-boilerplate>
+            body {
+                -webkit-animation: none;
+                -moz-animation: none;
+                -ms-animation: none;
+                animation: none
+            }
+        </style>
+    </noscript>
+
+    <link rel="canonical" href=".">
+    <title>My AMP Page</title>
+    <script type="application/ld+json">
+        {
+            "@context": "http://schema.org",
+            "@type": "Webpage",
+            "url": "https://www.example.com",
+            "name": "My Webpage",
+            "headline": "webpage title",
+            "description": "Here is the description",
+            "mainEntityOfPage": {
+                "@type": "WebPage",
+                "@id": "https://www.example.com"
+            },
+            "publisher": {
+                "@type": "Organization",
+                "name": "The Publisher",
+                "logo": {
+                    "url": "https://www.example.com/logo-blue.svg",
+                    "width": 175,
+                    "height": 60,
+                    "@type": "ImageObject"
+                }
+            },
+            "image": {
+                "@type": "ImageObject",
+                "url": "https://www.example.com/leader.png",
+                "width": 1200,
+                "height": 630
+            }
+        }
+    </script>
+</head>
+
+<body>
+    <h1>Hello World</h1>
+    <amp-img src="hero-img.jpg" width="1280" height="720" layout="responsive" alt="This is a hero image which should be loaded as fast as possible">
+    </amp-img>
+    <amp-analytics type="gtag" data-credentials="include">
+        <script type="application/json">
+            {
+                "vars": {
+                    "gtag_id": "UA-XXXXX-Y",
+                    "config": {
+                        "UA-XXXXX-Y": {
+                            "groups": "default"
+                        }
+                    }
+                }
+            }
+        </script>
+    </amp-analytics>
+</body>
+
+</html>
