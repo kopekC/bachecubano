74d2237c47c41e3cdb9fa67138f2cbda369a89b0
Erich Garcia Cruz <erichjosegarcia@gmail.com>
2019-07-12 12:28:55 -0400

    updated libraries

--- resources/views/blocks/hero.blade.php
+++ resources/views/blocks/hero.blade.php
@@ -5,7 +5,7 @@
         <div class="row d-flex justify-content-center">
             <div class="col-md-9 col-lg-9 col-xs-12 text-center">
                 <div class="contents">
-                    <h1 class="head-title">Busca anuncios entre +{{ $total_ads }}</h1>
+                    <h1 class="head-title">Busca entre +{{ $total_ads }} anuncios</h1>
                     <p>Compra y Vende Computadoras, Celulares, Casas, Carros, ofertas de empleo y todo lo que quieras en Cuba</p>
                     <div class="search-two-form">
                         <form class="search-two" action="{{ route('search') }}" method="get">
