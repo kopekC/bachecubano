2a1b1835527d5cc41fe99e471c2abf02d7f8edbf
Erich Garciam <ecruz@bachecubano.com>
2019-07-30 14:29:07 -0400

    login/register ok!

---
+++ resources/views/blocks/register.blade.php
@@ -0,0 +1,65 @@
+<!-- Content section Start -->
+<section class="register section-padding">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-5 col-md-12 col-xs-12">
+                <div class="register-form login-area">
+                    <h3>Crear mi cuenta en Bachecubano</h3>
+                    <form class="login-form" method="POST" action="{{ route('register') }}">
+                        @csrf
+
+                        <div class="form-group">
+                            <div class="input-icon">
+                                <i class="lni-user"></i>
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" placeholder="Nombre" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="input-icon">
+                                <i class="lni-envelope"></i>
+                                <input id="sender-email" type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Correo Electrónico" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="input-icon">
+                                <i class="lni-lock"></i>
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Contraseñe" name="password" required autocomplete="new-password">
+                                @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="input-icon">
+                                <i class="lni-lock"></i>
+                                <input id="password-confirm" type="password" class="form-control" placeholder="Repite la Contraseñe" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                        </div>
+                        <div class="form-group mb-3 text-center">
+                            <div class="checkbox">
+                                <input type="checkbox" name="rememberme" value="rememberme">
+                                <label>Aceptas Términos y Condiciones ok?</label>
+                            </div>
+                        </div>
+                        <div class="text-center">
+                            <button class="btn btn-common log-btn" type="submit">Registrarme</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Content section End -->
