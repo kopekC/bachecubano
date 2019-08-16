1d70d2201c4eaf229e3cebdf88450d4a799c6db9
Erich Garcia Cruz <erichjosegarcia@gmail.com>
2019-08-16 17:03:34 -0400

    API get_categories and GET Ads (without emails)

--- app/Http/Controllers/Api/AdController.php
+++ app/Http/Controllers/Api/AdController.php
@@ -5,26 +5,82 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Category;
+use App\Ad;
+
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Cache;
+
 class AdController extends Controller
 {
-    
-    public function get_categories() {
-
+    /**
+     * Get All Right Now Categories
+     * https://www.bachecubano.com/api/1.0/categories
+     * or
+     * httas://api.bachecubano.com/1.0/categories
+     */
+    public function get_categories()
+    {
+        $categories = Cache::remember('cached_categpories', 60 * 24 * 7, function () {
+            return Category::with(['description', 'stats', 'parent', 'childs'])->get();
+        });
+        return response()->json($categories);
     }
 
-    public function get_ads(Request $request, $category_id) {
+    /**
+     * Get Ads for specific Category
+     * https://www.bachecubano.com/api/1.0/ads/104
+     * or
+     * httas://api.bachecubano.com/1.0/ads/104
+     */
+    public function get_ads(Request $request, $category_id)
+    {
 
-        $query = Ad::query();
-        //Select All elements
-        $query->select('ads.*', 'ad_promos.promotype');
-        //Category Condition if subcategory or Super Category
-        if (isset($sub_category->category_id)) {
-            $query->where('category_id', $sub_category->category_id);
-        } else {
-            $sub_categories = Category::where('parent_id', '=', $super_category->category_id)->select('id')->get();
-            $query->whereIn('category_id', $sub_categories);
-        }
-        
+        $ads = Cache::remember('cached_ads_' . $category_id, 60, function () use ($category_id) {
 
+            $query = Ad::query();
+
+            //Select All elements
+            $query->select('ads.id', 'ads.user_id', 'ads.category_id', 'ads.updated_at', 'ads.price', 'ads.contact_name', 'ad_promos.promotype');
+
+            //Category Condition
+            $query->where('category_id', $category_id);
+
+            //With associated elements
+            $query->with(['description', 'resources']);
+
+            //Join for a correct ordering?
+            $query->leftjoin('ad_promos', 'ads.id', '=', 'ad_promos.ad_id');
+            //Minimal Price
+            if (null !== Input::get('min_price')) {
+                $query->when(request('min_price') >= 0, function ($q) {
+                    return $q->where('price', '>=', request('min_price', 0));
+                });
+            }
+            //Maximum Price
+            if (null !== Input::get('max_price')) {
+                $query->when(request('max_price') >= 0, function ($q) {
+                    return $q->where('price', '<=', request('max_price', 0));
+                });
+            }
+            //Search Query With on;y Photos ads
+            if (null !== Input::get('only_photos')) {
+                $query->when(request('only_photos') == 1, function ($q) {
+                    return $q->whereHas('resources');
+                });
+            }
+            //Order By PromoType and later as updated time
+            $query->orderBy('ad_promos.promotype', 'desc');
+            $query->latest();
+
+            //Activated parameters
+            $query->where('active', 1);
+            $query->where('enabled', 1);
+
+            //Paginate all this
+            return $query->paginate(50);
+        });
+
+        return response()->json($ads);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -11,11 +11,18 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
+| Within this group, the `/api` URI prefix is automatically applied so you do not need to manually apply it to every route in the file
+|
 */
 
 //MailGun Routes WebHook
 Route::group(['prefix' => 'mailgun',], function () {
     Route::post('widgets', 'MailgunWidgetsController@store');
+});
+
+Route::group(['prefix' => '1.0',], function () {
+    Route::get('categories', 'Api\AdController@get_categories')->name('api_get_categories');
+    Route::get('ads/{category_id}', 'Api\AdController@get_ads')->name('api_get_ads');
 });
 
 //Save Image from AJAX Calls and API implementation
