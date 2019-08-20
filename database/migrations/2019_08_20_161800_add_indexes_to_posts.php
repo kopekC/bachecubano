54ab01705c3a0663dee74601d33c9d7ef6d7f9d5
Erich Garcia Cruz <erichjosegarcia@gmail.com>
2019-08-20 12:20:13 -0400

    indexes to posts

---
+++ database/migrations/2019_08_20_161800_add_indexes_to_posts.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIndexesToPosts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->index('user_id');
+            $table->index('title');
+            $table->index('slug');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
+    }
+}
