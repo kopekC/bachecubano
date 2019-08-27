2890ee80ff6b05a94443758e6c948138ba48463a
Erich Garcia Cruz <erichjosegarcia@gmail.com>
2019-08-27 10:05:50 -0400

    Ban User

--- database/migrations/2019_08_27_022641_add_banned_until_to_users_table.php
+++ database/migrations/2019_08_27_022641_add_banned_until_to_users_table.php
@@ -26,7 +26,7 @@ class AddBannedUntilToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table - $table->dropColumn('banned_until');
         });
     }
 }
