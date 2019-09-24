d447cdc3b7dd89cf42d742aa4e3e93c4f8ac2c89
Erich Garcia Cruz <ecruz@bachecubano.com>
2019-09-24 19:58:07 -0400

    Telegram Done

--- app/Notifications/AdPromoted.php
+++ app/Notifications/AdPromoted.php
@@ -40,11 +40,13 @@ class AdPromoted extends Notification
     /**
      * Send this to Telegram Channel
      * <b>bold</b>, <strong>bold</strong>
-     *<i>italic</i>, <em>italic</em>
-     *<a href="http://www.example.com/">inline URL</a>
-     *<a href="tg://user?id=123456789">inline mention of a user</a>
-     *<code>inline fixed-width code</code>
-     *<pre>pre-formatted fixed-width code block</pre>
+     * <i>italic</i>, <em>italic</em>
+     * <a href="http://www.example.com/">inline URL</a>
+     * <a href="tg://user?id=123456789">inline mention of a user</a>
+     * <code>inline fixed-width code</code>
+     * <pre>pre-formatted fixed-width code block</pre>
+     *
+     * Add some analitycs to this
      */
     public function toTelegram($ad)
     {
