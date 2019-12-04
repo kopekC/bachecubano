c202c89d7ed150651e2230e1fd8f66fc0d0ef423
Erich Garcia Cruz <ecruz@bachecubano.com>
2019-12-03 22:52:47 -0500

    Begin LaChopi generation

---
+++ app/Http/Controllers/LaChopiGeneration.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+/**
+ * Class for LaChopi DB Generation
+ */
+class LaChopiGeneration extends Controller
+{
+
+    public function __construct()
+    {
+        //begin benchmark, and some prior optimizations
+    }
+
+    public function generate() {
+        //Get all category ID's
+
+        
+    }
+
+    public function getCategoryAds($category_id) {
+
+    }
+}
