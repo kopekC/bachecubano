8ac2599b998676fc9044f61f094fcd09c145b1c4
Erich Garcia Cruz <ecruz@bachecubano.com>
2019-12-25 20:20:47 -0500

    Custom Password Reset

---
+++ app/Notifications/ResetPassword.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+/**
+ * Custom Email reset password notification
+ * https://stackoverflow.com/questions/40574001/how-to-change-reset-password-email-subject-in-laravel
+ */
+class ResetPassword extends Notification
+{
+    use Queueable;
+
+    public $token;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject('Solicitud de contraseña olvidada en Bachecubano')
+            ->line('Está recibiendo este correo porque ha solicitado el reseteo de su contraseña en Bachecubano.')
+            ->action('Resetear contraseña', url('password/reset', $this->token))
+            ->line('Si usted no ha solicitado dicho reseteo, ignore este mensaje');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
