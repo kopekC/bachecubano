e131a373decbf27bab1425a6038bf5dc5a3da9c2
Erich Garcia Cruz <ecruz@bachecubano.com>
2020-01-21 09:50:37 -0500

    La Chopi tweak

--- app/Http/Controllers/Api/LachopigenerationController.php
+++ app/Http/Controllers/Api/LachopigenerationController.php
@@ -256,7 +256,7 @@ class LachopigenerationController extends Controller
     {
         //This is called every category ID, so retrieve ads from it
         $request = new Request();
-        $limit = 1000;
+        $limit = 10000;
         $latest_days = 7;
 
         return AdController::getAds($request, $category_id, $limit, $latest_days);
