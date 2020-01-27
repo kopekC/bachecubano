45fdd01df60b3b4d2bcce3f5633fba2dcfa23199
Erich Garcia Cruz <ecruz@bachecubano.com>
2020-01-27 00:23:02 -0500

    Imap config file

--- config/imap.php
+++ config/imap.php
@@ -1,7 +1,8 @@
 <?php
+
 return [
     'imap_server' => env('IMAP_SERVER', 'mail.bachecubano.com'),
     'imap_port' => env('IMAP_PORT', '143'),
-    'imap_user' => env('IMAP_USER', 'pepe'),
-    'imap_password' => env('IMAP_PASSWORD', 'conejito')
+    'imap_user' => env('IMAP_USER', ''),
+    'imap_password' => env('IMAP_PASSWORD', '')
 ];
