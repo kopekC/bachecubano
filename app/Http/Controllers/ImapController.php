17fd861ffb75e824850d244e149f452b2d60fb3d
Erich Garcia Cruz <ecruz@bachecubano.com>
2020-01-27 09:22:33 -0500

    get Messages Test

--- app/Http/Controllers/ImapController.php
+++ app/Http/Controllers/ImapController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Fetch\Server;
 
 class ImapController extends Controller
 {
@@ -15,9 +16,20 @@ class ImapController extends Controller
         $imap_port = config('imap.imap_port');
         $imap_user = config('imap.imap_user');
         $imap_password = config('imap.imap_password');
+
+        $this->driver = new Server($imap_server, $imap_port);
+
+        dump($this->driver);
+
+        $this->driver->setAuthentication($imap_user, $imap_password);
+
+        dump($this->driver);
     }
 
     public function imap_check()
     {
+        /** @var Message[] $message */
+        $messages = $this->driver->getMessages(10);
+        dump($messages);
     }
 }
--- config/imap.php
+++ config/imap.php
@@ -2,7 +2,7 @@
 
 return [
     'imap_server' => env('IMAP_SERVER', 'mail.bachecubano.com'),
-    'imap_port' => env('IMAP_PORT', '995'),
+    'imap_port' => env('IMAP_PORT', 143),
     'imap_user' => env('IMAP_USER', ''),
     'imap_password' => env('IMAP_PASSWORD', '')
 ];
