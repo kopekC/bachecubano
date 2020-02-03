fedac62259e0a0219953680e9d33e6dfe03ae569
Erich Garcia Cruz <ecruz@bachecubano.com>
2020-02-03 12:46:27 -0500

    home advertising

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -48,6 +48,13 @@
                 </tr>
             </thead>
             <tbody>
+
+                <!-- Explicit Horizontal Ad -->
+                <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-9876511577005081" data-ad-slot="2887444455" data-ad-format="auto" data-full-width-responsive="true"></ins>
+                <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                </script>
+
                 @if($popular_ads)
                 @foreach($popular_ads as $ad)
                 <tr data-category="active">
