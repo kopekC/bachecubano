827e378e9c2deaa8d2f1d6df67c07a87a03adc3f
Erich Garcia Cruz <ecruz@bachecubano.com>
2020-02-03 12:41:43 -0500

    horizontal advert on my ads

--- resources/views/user/ads.blade.php
+++ resources/views/user/ads.blade.php
@@ -49,6 +49,13 @@
                     </tr>
                 </thead>
                 <tbody>
+
+                    <!-- Explicit Horizontal Ad -->
+                    <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-9876511577005081" data-ad-slot="2887444455" data-ad-format="auto" data-full-width-responsive="true"></ins>
+                    <script>
+                        (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>
+
                     @if($my_ads)
                     @foreach($my_ads as $ad)
                     <tr data-category="active">
