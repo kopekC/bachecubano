b8371e6cad977a71da2a67f2fa41d6c6088cd380
Erich Garcia Cruz <ecruz@bachecubano.com>
2020-02-14 12:07:30 -0500

    new css

--- routes/web.php
+++ routes/web.php
@@ -43,8 +43,8 @@ Route::get('/redirect/{provider}', 'SocialController@redirect')->name('social_lo
 Route::get('/callback/{provider}', 'SocialController@callback')->name('social_callback');
 
 //CSS controller
-Route::get('/css/bch.css', 'WelcomeController@bachecubano_css')->name('bachecubano_css');
-Route::get('/js/bch.js', 'WelcomeController@bachecubano_js')->name('bachecubano_js');
+Route::get('/css/bch1.css', 'WelcomeController@bachecubano_css')->name('bachecubano_css');
+Route::get('/js/bch1.js', 'WelcomeController@bachecubano_js')->name('bachecubano_js');
 
 // Posts resourcfull controllers routes
 Route::get('/blog/create', 'PostController@create')->name('blog_post_create');
