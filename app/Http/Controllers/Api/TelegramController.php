5b2639940a0ebc972873f0ef6328a7f3eca79dd4
Erich Garcia Cruz <ecruz@bachecubano.com>
2020-02-18 11:00:29 -0500

    update dump

--- app/Http/Controllers/Api/TelegramController.php
+++ app/Http/Controllers/Api/TelegramController.php
@@ -15,6 +15,8 @@ class TelegramController extends Controller
 
     /**
      * My Bot info
+     * Already configured
+     * https://core.telegram.org/bots/inline
      */
     public function getme()
     {
@@ -30,15 +32,48 @@ class TelegramController extends Controller
     }
 
     /**
-     * Get Telegram updates
+     * Get Telegram updates via getUpdate method
+     * Migrar luego este endpoint a webhook ok?
+     * Obtained: https://core.telegram.org/bots/api#update
      */
     public function getupdates()
     {
         $updates = Telegram::getUpdates();
+
+        /**
+         * update_id
+         * message
+         * edited_message
+         * channel_post
+         * edited_channel_post
+         * inline_query
+         * chosen_inline_result
+         * callback_query
+         * shipping_query
+         * pre_checkout_query
+         * poll
+         * poll_answer
+         */
+
+        if(count($updates) > 0) {
+            foreach($updates as $update) {
+                dump($update);
+                dump($update['update_id']);
+                dump($update['message']);
+                dump($update['message']['message_id']);
+                dump($update['message']['from']);
+                dump($update['message']['date']);
+                dump($update['message']['text']);
+            }
+        }
+
         return (json_encode($updates));
     }
 
-    public function sendmessage()
+    /**
+     * Enviar mensaje de vuelta a sender
+     */
+    public function sendmessage($recipient, $text)
     {
         Telegram::sendMessage([
             'chat_id' => 'RECIPIENT_CHAT_ID',
