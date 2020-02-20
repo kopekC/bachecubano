f7336a217fb3665acaf4454d3fe5d2b940a8e23f
Erich Garcia Cruz <ecruz@bachecubano.com>
2020-02-19 21:46:54 -0500

    response object

--- app/Http/Controllers/Api/TelegramCommands/SearchCommand.php
+++ app/Http/Controllers/Api/TelegramCommands/SearchCommand.php
@@ -27,15 +27,6 @@ class SearchCommand extends Command
      */
     public function handle()
     {
-        //Input data here?
-        dump($this->getUpdate()->message);
-
-        $this->replyWithMessage(['text' => json_encode($this->getUpdate()->message)]);
-
-        // This will send a message using `sendMessage` method behind the scenes to
-        // the user/chat id who triggered this command.
-        // `replyWith<Message|Photo|Audio|Video|Voice|Document|Sticker|Location|ChatAction>()` all the available methods are dynamically
-        // handled when you replace `send<Method>` with `replyWith` and use the same parameters - except chat_id does NOT need to be included in the array.
-        $this->replyWithMessage(['text' => $this->getUpdate()->message->from->username]);
+        $this->replyWithMessage(['text' => json_encode($this->getUpdate()->message->text)]);
     }
 }
