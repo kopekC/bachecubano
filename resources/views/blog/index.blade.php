62c4566c34df0874aeb1beede87f204162bb7e3e
Erich Garcia Cruz <ecruz@bachecubano.com>
2020-03-20 00:26:49 -0400

    index

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -28,10 +28,12 @@
 <div id="content" class="section-padding">
     <div class="container">
         <div class="row">
+
             <div class="col-lg-8 col-md-12 col-xs-12">
 
                 @if(isset($posts) && $posts->count() > 0)
                 @foreach($posts as $blog_post)
+
                 <!-- Start Post -->
                 <div class="blog-post">
                     <!-- Post thumb -->
@@ -59,55 +61,10 @@
                     <!-- Post Content -->
                 </div>
                 <!-- End Post -->
+
                 @endforeach
                 @endif
-
-                {{--
-                <!-- Start Post -->
-                <div class="blog-post quote-post">
-                    <div class="quote-wrap">
-                        <i class="fa fa-quote-left"></i>
-                        <blockquote class="text-center">
-                            Crafting visually stunning memorable experiences
-                            <br>
-                            for web and interfaces.
-                        </blockquote>
-                        <i class="fa fa-quote-right"></i>
-                    </div>
-                </div>
-                <!-- End Post -->
-
-                <!-- Start Post -->
-                <div class="blog-post video-post">
-                    <!-- Post thumb -->
-                    <div class="post-thumb">
-                        <div class="video-wrapper">
-                            <iframe width="100%" height="315" src="https://www.youtube.com/embed/qighCE8WfBk" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                        </div>
-                    </div>
-                    <!-- End Post post-thumb -->
-
-                    <!-- Post Content -->
-                    <div class="post-content">
-                        <ul class="list-inline cat-meta">
-                            <li class="tr-cats"><a href="#">Video</a></li>
-                        </ul>
-                        <h2 class="post-title"><a href="single-post.html">Exercitation Photo Booth</a></h2>
-                        <div class="meta">
-                            <span class="meta-part"><a href="#"><i class="lni-user"></i> Clasihub</a></span>
-                            <span class="meta-part"><a href="#"><i class="lni-alarm-clock"></i> June 21, 2018</a></span>
-                            <span class="meta-part"><a href="#"><i class="lni-folder"></i> Sticky</a></span>
-                            <span class="meta-part"><a href="#"><i class="lni-comments-alt"></i> 1 Comments</a></span>
-                        </div>
-                        <div class="entry-summary">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis soluta libero molestiae, id reiciendis ipsum consequuntur odit sapiente accusantium odio. Esse nemo quos quaerat illo! Enim saepe impedit distinctio, placeat. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate voluptatum dolores mollitia consequatur velit, veritatis in pariatur ducimus numquam ipsa iusto! Rem eveniet dolorum, quisquam neque beatae quas ea tenetur!</p>
-                        </div>
-                        <a href="single-post.html" class="btn btn-common btn-rm">Read More</a>
-                    </div>
-                    <!-- Post Content -->
-                </div>
-                <!-- End Post -->
-                --}}
+                
             </div>
 
             <!-- Start Pagination -->
